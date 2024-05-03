34ece7f4303716dd18e9bd5eb32ff5c65f709767
Tasnim Chowdhury <tasnimchowdhurynyc@gmail.com>
2024-05-02 21:42:06 -0400

    styles

--- src/components/MealDetails.jsx
+++ src/components/MealDetails.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMealByName } from '../services/mealServices';
 import '../styles/MealDetails.scss';
-
 const MealDetails = () => {
     const { mealName } = useParams();
     const [meal, setMeal] = useState(null);
@@ -37,30 +36,28 @@ const MealDetails = () => {
                         <h1 className="meal-title">{meal.mealName}</h1>
                         <h2 className="meal-type">Type: {meal.mealType || 'No type specified'}</h2>
                     </div>
-                    {meal.recipe ? (
-                        <div className="recipe-content">
-                            {meal.recipe.ingredients && meal.recipe.ingredients.length > 0 ? (
-                                <div className="ingredients">
-                                    <h4>Ingredients</h4>
-                                    <ul className="ingredient-list">
-                                        {meal.recipe.ingredients.map((ingredient, index) => (
-                                            <li key={index}>{ingredient.name} - {ingredient.amount} {ingredient.unit}</li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            ) : <p>No ingredients listed.</p>}
-                            {meal.recipe.steps && meal.recipe.steps.length > 0 ? (
-                                <div className="steps">
-                                    <h4>Steps</h4>
-                                    <ol className="step-list">
-                                        {meal.recipe.steps.map((step, index) => (
-                                            <li key={index}>{step}</li>
-                                        ))}
-                                    </ol>
-                                </div>
-                            ) : <p>No cooking steps provided.</p>}
-                        </div>
-                    ) : <p>No recipe details available.</p>}
+                    <div className="recipe-content">
+                        {meal.recipe && meal.recipe.ingredients && (
+                            <div className="ingredients">
+                                <h4>Ingredients</h4>
+                                <ul className="ingredient-list">
+                                    {meal.recipe.ingredients.map((ingredient, index) => (
+                                        <li key={index}>{ingredient.name}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+                        {meal.recipe && meal.recipe.steps && (
+                            <div className="steps">
+                                <h4>Steps</h4>
+                                <ol className="step-list">
+                                    {meal.recipe.steps.map((step, index) => (
+                                        <li key={index}>{step}</li>
+                                    ))}
+                                </ol>
+                            </div>
+                        )}
+                    </div>
                 </div>
             ) : <p>Meal not found.</p>}
         </div>
