eb0cab0466ee8500955b7e8cc106101f932f0da6
Tasnim Chowdhury <tasnimchowdhurynyc@gmail.com>
2024-05-03 19:12:46 -0400

    editing

--- src/components/EditMealDetails.jsx
+++ src/components/EditMealDetails.jsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { fetchMealByName, updateMeal } from '../services/mealServices';
+import viewIcon from '../img/view.png';
+import '../styles/MealDetails.scss';
 
 const EditMealDetails = () => {
     const { mealName } = useParams();
@@ -9,6 +11,9 @@ const EditMealDetails = () => {
     const [meal, setMeal] = useState({});
     const [ingredientInput, setIngredientInput] = useState('');
 
+    const handleView = () => {
+        navigate(`/meal-details/${mealName}`);
+    };
 
     useEffect(() => {
         fetchMealByName(decodeURIComponent(mealName), (mealData) => {
@@ -36,34 +41,46 @@ const EditMealDetails = () => {
     };
 
     return (
-        <form onSubmit={handleSave}>
-            <h1>{meal.mealName || 'No Meal Name'}</h1>
-            <select 
-                value={meal.mealType || ''} 
-                onChange={(e) => setMeal({ ...meal, mealType: e.target.value })}
-            >
-                <option value="">Select Meal Type</option>
-                <option value="breakfast">Breakfast</option>
-                <option value="lunch">Lunch</option>
-                <option value="dinner">Dinner</option>
-                <option value="snack">Snack</option>
-            </select>
-            <input 
-                type="text"
-                value={ingredientInput}
-                onChange={(e) => setIngredientInput(e.target.value)}
-                placeholder="Enter ingredients separated by semicolons"
-            />
-            <textarea
-                value={meal.recipe ? meal.recipe.steps.join('\n') : ''}
-                onChange={(e) => setMeal({
-                    ...meal,
-                    recipe: { ...meal.recipe, steps: e.target.value.split('\n') }
-                })}
-            />
-            
-            <button type="submit">Save</button>
-        </form>
+        <div className="edit-meal-details-container">
+            <form onSubmit={handleSave}>
+                <div className="edit-header">
+                <h1>{meal.mealName || 'No Meal Name'}</h1>
+                <button onClick={handleView} className="edit-button">
+                    <img src={viewIcon} alt="View Meal" />
+                </button>
+                </div>
+                <div className="edit-type">
+                <select 
+                    value={meal.mealType || ''} 
+                    onChange={(e) => setMeal({ ...meal, mealType: e.target.value })}
+                >
+                    <option value="">Select Meal Type</option>
+                    <option value="breakfast">Breakfast</option>
+                    <option value="lunch">Lunch</option>
+                    <option value="dinner">Dinner</option>
+                    <option value="snack">Snack</option>
+                </select>
+                </div>
+                <div className ="edit-ingredients">
+                <input 
+                    type="text"
+                    value={ingredientInput}
+                    onChange={(e) => setIngredientInput(e.target.value)}
+                    placeholder="Enter ingredients separated by semicolons"
+                />
+                </div>
+                <div className ="edit-steps">
+                <textarea
+                    value={meal.recipe ? meal.recipe.steps.join('\n') : ''}
+                    onChange={(e) => setMeal({
+                        ...meal,
+                        recipe: { ...meal.recipe, steps: e.target.value.split('\n') }
+                    })}
+                />
+                </div>
+                <button type="submit">Save</button>
+            </form>
+        </div>
     );
 };
 
